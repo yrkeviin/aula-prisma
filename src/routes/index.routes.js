dd853a898ea4b7b491b545c0dc9cc5119f47e43c
yrkeviin <kevineziquiellima160@gmail.com>
2025-09-30 10:26:21 -0300

    Feat: Finish project

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -1,9 +1,11 @@
 import express from "express";
 
 // Importar todas as rotas
+import movieRouter from "./movies.routes.js"
 
 const router = express.Router();
 
 // Rotas públicas
+router.use("/movies", movieRouter);
 
 export default router;
--- src/server.js
+++ src/server.js
@@ -1,6 +1,8 @@
 import express from "express";
 import { config } from "dotenv";
 
+import router from "./routes/index.routes.js";
+
 config(); // Carrega variáveis de ambiente do arquivo .env
 const port = process.env.PORT || 5000;
 
@@ -9,10 +11,7 @@ const app = express();
 
 app.use(express.json()); // Parse de JSON
 
-// Rota base para verificar se o servidor está rodando
-app.get("/", (req, res) => {
-    res.json({ message: "API de Coleção de Animes funcionando!" });
-});
+app.use("/", router);
 
 // Iniciar o servidor
 app.listen(port, () => {
