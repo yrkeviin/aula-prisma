95b54e1748494467e82460a1bbe985fa8169c4a6
yrkeviin <kevineziquiellima160@gmail.com>
2025-09-30 09:49:34 -0300

    Feat: Create movie routes complete

--- src/routes/movies.routes.js
+++ src/routes/movies.routes.js
@@ -1,5 +1,9 @@
 import express from "express";
+import MovieController from "../controllers/movies.controller.js"
 
 const movieRouter = express.Router();
+
+movieRouter.get("/", MovieController.findAll);
+movieRouter.post("/", MovieController.create);
 
 export default movieRouter;
